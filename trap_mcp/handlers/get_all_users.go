f09d40bfd4b3812fad5e5ca19da02bd4b591ae2f
comavius <comavius@proton.me>
2025-06-22 00:28:33 +0900

    trivial fixes on mcp usecases

--- trap_mcp/handlers/get_all_users.go
+++ trap_mcp/handlers/get_all_users.go
@@ -11,6 +11,10 @@ import (
 func GetAllUsrsTool() mcp.Tool {
 	tool := mcp.NewTool("getAllUsers",
 		mcp.WithDescription("Get all traP users (excluding bot or webhook users)"),
+		mcp.WithBoolean("includeSuspended",
+			mcp.Description("Whether to include suspended users"),
+			mcp.DefaultBool(false),
+		),
 	)
 	return tool
 }
@@ -18,6 +22,10 @@ func GetAllUsrsTool() mcp.Tool {
 func GetAllUsersHandler(ctx context.Context, request mcp.CallToolRequest) (*mcp.CallToolResult, error) {
 	traq_client := clients.GetTraqClient()
 	getReq := traq_client.UserApi.GetUsers(ctx)
+	useSuspended, err := request.RequireBool("includeSuspended")
+	if err == nil {
+		getReq = getReq.IncludeSuspended(useSuspended)
+	}
 	res, _, err := getReq.Execute()
 	if err != nil {
 		return mcp.NewToolResultError(err.Error()), nil
