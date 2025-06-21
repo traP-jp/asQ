63ab8cce397ca66f30f4005caf4f78235e4384bc
takino0309 <takino0309@gmail.com>
2025-06-21 17:35:35 +0900

    stampのキャッシュ

---
+++ trap_mcp/repository/stamp_cache.go
@@ -0,0 +1,37 @@
+package repository
+
+import (
+	"context"
+	"time"
+
+	"github.com/traP-jp/h25s_05/trap_mcp/clients"
+)
+
+var (
+	idToStampName      map[string]string
+	idToStampUpdatedAt time.Time = time.UnixMicro(0)
+)
+
+func updateStampCache(ctx context.Context) error {
+	if time.Since(idToStampUpdatedAt) < 10*time.Minute {
+		return nil
+	}
+	traqClient := clients.GetTraqClient()
+	channels, _, err := traqClient.StampApi.GetStamps(ctx).Execute()
+	if err != nil {
+		return err
+	}
+	idToStampName = make(map[string]string)
+	for _, channel := range channels {
+		idToStampName[channel.Id] = channel.Name
+	}
+	idToStampUpdatedAt = time.Now()
+	return nil
+}
+
+func GetIdToStamp(ctx context.Context) (map[string]string, error) {
+	if err := updateStampCache(ctx); err != nil {
+		return nil, err
+	}
+	return idToStampName, nil
+}
