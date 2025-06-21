9cb648b6e160046126529240cbb9ce8dbdd87917
takino0309 <takino0309@gmail.com>
2025-06-21 16:08:39 +0900

    return修正

--- trap_mcp/repositry/get-userID.go
+++ trap_mcp/repositry/get-userID.go
@@ -12,18 +12,18 @@ var (
 	updatedAt     time.Time = time.UnixMicro(0)
 )
 
-func updateCache(ctx context.Context) (map[string]string, error) {
+func updateCache(ctx context.Context) error {
 	traq_client := clients.GetTraqClient()
 	users, _, err := traq_client.UserApi.GetUsers(ctx).IncludeSuspended(true).Execute()
 	if err != nil {
-		return nil, err
+		return err
 	}
 	name_to_id := make(map[string]string)
 	for _, v := range users {
 		name_to_id[v.Name] = v.Id
 	}
 	nameToIdCache = name_to_id
-	return name_to_id, nil
+	return nil
 }
 
 func GetUserToId(ctx context.Context) (map[string]string, error) {
