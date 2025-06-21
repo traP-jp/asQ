8d4de953a706774864ddef76365e47973e8d2a5f
takino0309 <takino0309@gmail.com>
2025-06-21 15:39:37 +0900

    getUser追加

--- trap_mcp/handlers/get_user.go
+++ trap_mcp/handlers/get_user.go
@@ -1,10 +1,15 @@
 package handlers
 
 import (
+	"context"
+	"encoding/json"
+
 	"github.com/mark3labs/mcp-go/mcp"
+	"github.com/traP-jp/h25s_05/trap_mcp/clients"
+	"github.com/traP-jp/h25s_05/trap_mcp/repositry"
 )
 
-func GetUser() mcp.Tool {
+func GetUserTool() mcp.Tool {
 	tool := mcp.NewTool("getUser",
 		mcp.WithDescription("Get user"),
 		mcp.WithString("name",
@@ -13,4 +18,33 @@ func GetUser() mcp.Tool {
 		),
 	)
 	return tool
+}
+
+func GetUserHandler(ctx context.Context, request mcp.CallToolRequest) (*mcp.CallToolResult, error) {
+	traq_client := clients.GetTraqClient()
+	name, err := request.RequireString("name")
+	if err != nil {
+		return mcp.NewToolResultError(err.Error()), nil
+	}
+	name_to_id, err := repositry.GetUserToId(ctx)
+	if err != nil {
+		return mcp.NewToolResultError(err.Error()), nil
+	}
+	id, ok := name_to_id[name]
+	if !ok {
+		return mcp.NewToolResultText("Username not found"), nil
+	}
+	getReq := traq_client.UserApi.GetUser(ctx, id)
+
+	//searchReq := traq_client.UserApi.GetUser(ctx)
+
+	res, _, err := getReq.Execute()
+	if err != nil {
+		return mcp.NewToolResultError(err.Error()), nil
+	}
+	jsonBytes, err := json.Marshal(res)
+	if err != nil {
+		return mcp.NewToolResultError(err.Error()), nil
+	}
+	return mcp.NewToolResultText(string(jsonBytes)), nil
 }
