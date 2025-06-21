f86299e04b5602829ca920ea23bccc61d9abc673
takino0309 <takino0309@gmail.com>
2025-06-21 16:03:43 +0900

    ユーザーID取得にキャッシュ追加

--- trap_mcp/repositry/get-userID.go
+++ trap_mcp/repositry/get-userID.go
@@ -2,11 +2,17 @@ package repositry
 
 import (
 	"context"
+	"time"
 
 	"github.com/traP-jp/h25s_05/trap_mcp/clients"
 )
 
-func GetUserToId(ctx context.Context) (map[string]string, error) {
+var (
+	nameToIdCache map[string]string
+	updatedAt     time.Time = time.UnixMicro(0)
+)
+
+func updateCache(ctx context.Context) (map[string]string, error) {
 	traq_client := clients.GetTraqClient()
 	users, _, err := traq_client.UserApi.GetUsers(ctx).IncludeSuspended(true).Execute()
 	if err != nil {
@@ -16,6 +22,15 @@ func GetUserToId(ctx context.Context) (map[string]string, error) {
 	for _, v := range users {
 		name_to_id[v.Name] = v.Id
 	}
-
+	nameToIdCache = name_to_id
 	return name_to_id, nil
+}
+
+func GetUserToId(ctx context.Context) (map[string]string, error) {
+	now := time.Now()
+	if now.Sub(updatedAt) > time.Hour {
+		updateCache(ctx)
+	}
+
+	return nameToIdCache, nil
 }
