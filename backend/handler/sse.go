fa96d8dfa5ea9c1b94e9c59ee6ea65b95eacc5f3
pirosiki197 <lingmukangtai825@gmail.com>
2025-06-21 15:26:20 +0900

    Implement stream AI response

---
+++ backend/handler/sse.go
@@ -0,0 +1,98 @@
+package handler
+
+import (
+	"context"
+	"encoding/json"
+	"log/slog"
+	"net/http"
+
+	"github.com/google/uuid"
+	"github.com/labstack/echo/v4"
+)
+
+type AIResponseMessage struct {
+	Message string `json:"message"`
+}
+
+type AIResponseStartMessage struct {
+	CharacterID uuid.UUID `json:"characterId"`
+}
+
+func (h *Handler) StreamAIResponse(c echo.Context) error {
+	id, err := uuid.Parse(c.Param("id"))
+	if err != nil {
+		return c.JSON(http.StatusBadRequest, map[string]string{"error": "Invalid ID format"})
+	}
+
+	w := c.Response()
+	sse := StartSSE(w)
+
+	err = sse.WriteJSON("start", AIResponseStartMessage{CharacterID: uuid.New()}) // TODO: replace with actual character ID
+	if err != nil {
+		return c.JSON(http.StatusInternalServerError, map[string]string{"error": "Failed to write SSE start message"})
+	}
+
+	ctx, cancel := context.WithCancel(context.Background())
+	defer cancel()
+	stream, err := h.llmsvc.Subscribe(ctx, id)
+	if err != nil {
+		slog.Error("Failed to subscribe to AI response stream", "error", err)
+		return c.JSON(http.StatusInternalServerError, map[string]string{"error": "Failed to subscribe to AI response stream"})
+	}
+	for data := range stream {
+		err := sse.WriteJSON("data", AIResponseMessage{Message: data.TextDelta})
+		if err != nil {
+			return c.JSON(http.StatusInternalServerError, map[string]string{"error": "Failed to write SSE data"})
+		}
+	}
+
+	sse.WriteMessage("close", "Stream closed")
+
+	<-c.Request().Context().Done()
+
+	return nil
+}
+
+type SSEWriter struct {
+	w http.ResponseWriter
+}
+
+func StartSSE(w http.ResponseWriter) *SSEWriter {
+	w.Header().Set("Content-Type", "text/event-stream")
+	w.Header().Set("Cache-Control", "no-cache")
+	w.Header().Set("Connection", "keep-alive")
+
+	return &SSEWriter{w}
+}
+
+func (sse *SSEWriter) WriteJSON(event string, v any) error {
+	if _, err := sse.w.Write([]byte("event: " + event + "\n")); err != nil {
+		return err
+	}
+	if _, err := sse.w.Write([]byte("data: ")); err != nil {
+		return err
+	}
+	if err := json.NewEncoder(sse.w).Encode(v); err != nil {
+		return err
+	}
+	if _, err := sse.w.Write([]byte("\n\n")); err != nil {
+		return err
+	}
+	sse.w.(http.Flusher).Flush()
+	return nil
+}
+
+func (sse *SSEWriter) WriteMessage(event string, s string) error {
+	if _, err := sse.w.Write([]byte("event: " + event + "\n")); err != nil {
+		return err
+	}
+	if _, err := sse.w.Write([]byte("data: ")); err != nil {
+		return err
+	}
+	if _, err := sse.w.Write([]byte(s)); err != nil {
+		return err
+	}
+	sse.w.Write([]byte("\n\n"))
+	sse.w.(http.Flusher).Flush()
+	return nil
+}
