de45969eb038a7ad8be8a996429f66d711e9b033
pirosiki197 <lingmukangtai825@gmail.com>
2025-06-21 09:45:22 +0900

    Add db to handler

--- backend/go.mod
+++ backend/go.mod
@@ -2,7 +2,10 @@ module github.com/traP-jp/h25s_05
 
 go 1.24.3
 
-require github.com/labstack/echo/v4 v4.13.4
+require (
+	github.com/jmoiron/sqlx v1.4.0
+	github.com/labstack/echo/v4 v4.13.4
+)
 
 require (
 	github.com/labstack/gommon v0.4.2 // indirect
--- backend/handler/handler.go
+++ backend/handler/handler.go
@@ -1,8 +1,19 @@
 package handler
 
-import "github.com/labstack/echo/v4"
+import (
+	"github.com/jmoiron/sqlx"
+	"github.com/labstack/echo/v4"
+)
 
-type Handler struct{}
+type Handler struct {
+	db *sqlx.DB
+}
+
+func NewHandler(db *sqlx.DB) *Handler {
+	return &Handler{
+		db: db,
+	}
+}
 
 func (h *Handler) SetUpRoutes(api *echo.Group) {
 	api.GET("/ping", func(c echo.Context) error {
