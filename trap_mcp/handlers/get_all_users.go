5cb99ded33afeb2b7b8b81b8c7e32b0ab41c7031
takino0309 <takino0309@gmail.com>
2025-06-21 14:12:15 +0900

    ユーザー情報をとるハンドラー

--- trap_mcp/handlers/get_all_users.go
+++ trap_mcp/handlers/get_all_users.go
@@ -1,7 +1,10 @@
 package handlers
 
 import (
+	"context"
+
 	"github.com/mark3labs/mcp-go/mcp"
+	"github.com/traP-jp/h25s_05/trap_mcp/clients"
 )
 
 func GetAllUsrs() mcp.Tool {
@@ -9,4 +12,15 @@ func GetAllUsrs() mcp.Tool {
 		mcp.WithDescription("Get all traP users"),
 	)
 	return tool
+}
+
+func GetAllUsersHandler(ctx context.Context, request mcp.CallToolRequest) (*mcp.CallToolResult, error) {
+	traq_client := clients.GetTraqClient()
+	getReq := traq_client.UserApi.GetUsers(ctx)
+	res, _, err := getReq.Execute()
+	if err != nil {
+		return mcp.NewToolResultError(err.Error()), nil
+	}
+
+	return mcp.NewToolResultText(res[0].Name), nil
 }
