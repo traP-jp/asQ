b9b24855729b580692f51cc7fc48eb798f48a7f0
takino0309 <takino0309@gmail.com>
2025-06-21 12:13:57 +0900

    検索ツール

--- trap_mcp/handlers/search.go
+++ trap_mcp/handlers/search.go
@@ -6,10 +6,22 @@ import (
 
 func SerchTool() mcp.Tool {
 	tool := mcp.NewTool("search",
-		mcp.WithDescription("Search word"),
+		mcp.WithDescription("Search"),
 		mcp.WithString("word",
 			mcp.Required(),
 			mcp.Description("search word"),
+		),
+		mcp.WithString("to",
+			mcp.Required(),
+			mcp.Description("username whose mentioned"),
+		),
+		mcp.WithString("from",
+			mcp.Required(),
+			mcp.Description("username that sent message"),
+		),
+		mcp.WithString("bot",
+			mcp.Required(),
+			mcp.Description("whether it is bot"),
 		),
 	)
 	return tool
