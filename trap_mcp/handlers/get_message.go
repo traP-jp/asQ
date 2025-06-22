66becc7178be4336350ab4b7e2838e15c7fdf9f3
takino0309 <takino0309@gmail.com>
2025-06-22 11:20:53 +0900

    messageIDからcontentをとる

---
+++ trap_mcp/handlers/get_message.go
@@ -0,0 +1,30 @@
+package handlers
+
+import (
+	"context"
+
+	"github.com/mark3labs/mcp-go/mcp"
+	"github.com/traP-jp/h25s_05/trap_mcp/clients"
+)
+
+func GetMessageTool() mcp.Tool {
+	tool := mcp.NewTool("getMessage",
+		mcp.WithDescription("Get message from ID"),
+		mcp.WithString("messageId",
+			mcp.Description("Message ID to get"),
+			mcp.DefaultString(""),
+		),
+	)
+	return tool
+}
+
+func GetMessageHandler(ctx context.Context, request mcp.CallToolRequest) (*mcp.CallToolResult, error) {
+	traq_client := clients.GetTraqClient()
+	messageId, err := request.RequireString("messageId")
+	getReq := traq_client.MessageApi.GetMessage(ctx, messageId)
+	res, _, err := getReq.Execute()
+	if err != nil {
+		return mcp.NewToolResultError(err.Error()), nil
+	}
+	return mcp.NewToolResultText(res.Content), nil
+}
